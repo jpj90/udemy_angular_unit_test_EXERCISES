c34999e0978dc43b20a96e317868d451b14e6d3d
James Janse <jpauljanse@gmail.com>
2019-02-19 15:27:56 +0100

    fixed app bugs for setup

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,26 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { LikeComponent } from './2- like-component/like.component'
+import { VoterComponent } from './3- voter-component/voter.component';
+import { UsersComponent } from './4- user-component/users.component';
+import { UserService } from './4- user-component/user.service';
+import { TextSummaryPipe } from './1- text-summary-pipe/text-summary.pipe';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LikeComponent,
+    VoterComponent,
+    UsersComponent,
+    TextSummaryPipe
   ],
   imports: [
     BrowserModule
   ],
-  providers: [],
+  providers: [
+    UserService
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
